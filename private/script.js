df64763eacd93fc63a74a364dea935d8a5d230e0
attaeht <attaeht+xpstools@gmail.com>
2023-01-14 17:11:48 +0000

    scirpt

---
+++ private/script.js
@@ -0,0 +1,18 @@
+var terminal=document.getElementById('terminalinput');
+
+document.getElementById('terminalinput').onkeypress = function(e) {
+    if (!e) e = window.event;
+    var keyCode = e.code || e.key;
+    if (keyCode == 'Enter'){
+        if(terminal.value == objPeople[0].terminal) {
+			console.log("attempting to redirect user...");
+            window.location.href="taj.txt";
+		}
+    }
+}
+
+var objPeople = [
+	{ // Object @ 0 index
+		terminal: "cha0s"
+	}
+]
