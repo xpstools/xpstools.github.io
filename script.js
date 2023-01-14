47c08393f9a7d230c977e0988d4250e772fa6d06
attaeht <attaeht@gmail.com>
2023-01-14 11:58:22 +0000

    login

---
+++ script.js
@@ -0,0 +1,26 @@
+/* ======================================================================
+  Author Custom JavaScript
+====================================================================== */
+// Loop through Array of Objects
+
+var objPeople = [
+	{ // Object @ 0 index
+		password: "itz_taj:Bluemoon23"
+	}
+]
+
+function getInfo() {
+	var password = document.getElementById('key').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(password == objPeople[i].password) {
+			console.log(password + " logging in...")
+            window.location.replace("https://google.com")
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	console.log("incorrect key")
+    alert("Incorrect key entered... Please recheck your key or ask the administrator for a reset or new one.")
+}
